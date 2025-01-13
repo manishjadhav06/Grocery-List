577e273c8ca7594e714416ed22dc0e879dca3b54
manishjadhav06 <manishjadhav.occ@gmail.com>
2025-01-13 10:48:33 +0530

    event handler

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Content = () => {
+  const handleNameChange =()=>{
+    const names = ['jadhav','Manish','joshi','shinde'];
+    const int = Math.floor(Math.random()*3);
+    return names[int];
+  }
+  const handleClick=()=> console.log("You clicked!");
+  const handleClick2=(name)=> console.log(`${name} was clicked`);
+
   return (
     <main>
-        
+      <p>
+        Hello {handleNameChange()}!
+      </p>
+      <button onClick={handleClick}>Click here</button>
+      <button onClick={()=>handleClick2("Manish")}>Click here 2</button>
     </main>
   )
 }
